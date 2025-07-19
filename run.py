4012ecc63d0e6242e5a6d19fe10204d0541e73ca
mm413 <lcq225@163.com>
2025-07-19 14:08:57 +0800

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from waitress import serve
 from app_waitress import app
 
 if __name__ == '__main__':
-    print("Starting server on http://0.0.0.0:8000")
-    serve(app, host='0.0.0.0', port=8000)
+    print("Starting server on http://0.0.0.0:5000")
+    serve(app, host='0.0.0.0', port=5000)
